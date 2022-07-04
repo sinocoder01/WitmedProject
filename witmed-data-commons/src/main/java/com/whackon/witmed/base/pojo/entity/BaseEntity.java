65ca951b7fae21fdb6581af4ab744c6e966aee5f
Arthur <sinocoder@163.com>
2022-07-04 16:30:17 +0800

    添加 Witmed data Commons 信息

--- witmed-data-commons/src/main/java/com/whackon/witmed/base/pojo/entity/BaseEntity.java
+++ witmed-data-commons/src/main/java/com/whackon/witmed/base/pojo/entity/BaseEntity.java
@@ -1,10 +1,27 @@
 package com.whackon.witmed.base.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * <b>基础实体信息</b>
  *
  * @author Arthur
  * @date 2022/7/2
  */
-public class BaseEntity {
+@Data
+public class BaseEntity implements Serializable {
+	private static final long serialVersionUID = 377806458505909427L;
+	private String status;                      // 系统状态：Y-启用，N-禁用
+	@TableField("createdBy")
+	private String createdBy;                   // 创建人
+	@TableField("createTime")
+	private Date createTime;                    // 创建时间
+	@TableField("modifiedBy")
+	private String modifiedBy;                  // 修改人
+	@TableField("modifiedTime")
+	private Date modifiedTime;                  // 修改时间
 }
